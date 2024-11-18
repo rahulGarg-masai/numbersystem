b1fcf2d29441141867330059e96e3fbd15335fd9
Rahul Garg <lantern5320@gmail.com>
2024-11-18 17:11:11 +0530

    Number System project with odd even , prime number , min max and sum calculations with proper README file

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-#numbersystem project
-
-##Description
-
-This project includes javascript code to:
- 
- *Identify even and odd numbers from a given input array.("Index.js")
- *Find prime numbers from a given array.("app.js")
- *Calculate the maximum , minimum and sum of the prime numbers found.
-
-## Answers
- *Index.js -
-const numbers = [3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18];
-for (let i=0;i<numbers.length ;i++){
-    if(numbers[i]%2===0){
-        console.log(numbers[i] + "is even");
-    }
-    else {
-        console.log(numbers[i] + "is odd");
-    }
-}
-
-*app.js -
 const numbers = [
     19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
   ];
@@ -72,48 +49,3 @@ for (let i=0;i<primeArray.length ;i++){
     console.log("Minimum" , min);
     console.log("Sum" , sum);
 
-
-##Expected Outputs
-
-*Index.js - 
-
-3is odd
-7is odd
-2is even
-15is odd
-9is odd
-20is even
-5is odd
-14is even
-12is even
-1is odd
-8is even
-11is odd
-6is even
-19is odd
-4is even
-10is even
-17is odd
-13is odd
-16is even
-18is even
-
-
-*app.js -
-
-Prime Numbers: [
-  19, 23, 13, 31, 29,
-   2, 11, 17,  5
-]
-Odd Numbers: [
-  19, 23, 13, 31, 29,
-  35, 11, 17,  5,  9,
-  27
-]
-Even Numbers: [
-  4, 16, 28,  8, 14,
-  6,  2, 12, 30
-]
-Maximum 31
-Minimum 2
-Sum 150
